0cb2741a06419625bfe29216fbd23882543ba95c
Admin <Admin@DESKTOP-P2TNP1V>
2025-11-18 00:27:52 +0500

    added a constructor

--- Battle_with_a_monster/Monster.cs
+++ Battle_with_a_monster/Monster.cs
@@ -8,7 +8,15 @@ namespace Battle_with_a_monster
 {
     internal class Monster : Character
     {
+        public Monster() 
+        {
+            
+        }
 
-
+        public Monster(int newHealth, int newMaxHealth, string newName, int newMinStrength, int newMaxStrength) 
+            : base(newHealth, newMaxHealth, newName, newMinStrength, newMaxStrength)
+        {
+            
+        }
     }
 }
